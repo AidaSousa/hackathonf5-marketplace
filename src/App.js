1e408f3e77c7277c4c7f8153e6c5f21b9d9ed5ba
Ashur19 <iglesiasjota1911@gmail.com>
2023-02-22 19:51:43 +0100

    Carousel 3rd commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,7 @@
-/* eslint-disable jsx-a11y/alt-text */
-import CarouselItem from "./components/atoms/carouselItem";
-import Carousel from "./components/molecules/carousel";
-import slider1 from "./Img/Slider1.jpg"
-import slider2 from "./Img/Slider2.jpg"
-import slider3 from "./Img/Slider3.jpg"
-
-
 function App() {
   return (
     <div>
-      <Carousel>
-        <CarouselItem><img src={slider1} style={{width: "100%"}}/></CarouselItem>
-        <CarouselItem><img src={slider2} style={{width: "100%"} }/></CarouselItem>
-        <CarouselItem><img src={slider3} style={{width: "100%"} }/></CarouselItem>
-      </Carousel>
+
     </div>
   );  
 }
