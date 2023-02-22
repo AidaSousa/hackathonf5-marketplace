19d373e3eeda78c6a02254fb8e82bb5e45631b0b
AldaraMG~ <almaga71@gmail.com>
2023-02-22 20:13:51 +0100

    app.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,7 @@ import Navbar from "./components/molecules/Navbar";
 import Footer from "./components/molecules/Footer";
 
 function App() {
-  return (
-    
-    <><Navbar />
-    <Footer /></>
-  );
+ 
 }
 
 export default App;
