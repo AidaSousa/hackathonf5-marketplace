3d6ef57c66ec2b4eaf22530833ad3e48b0fe17dd
Ashur19 <iglesiasjota1911@gmail.com>
2023-02-22 19:20:23 +0100

    Carousel commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+/* eslint-disable jsx-a11y/alt-text */
+import CarouselItem from "./components/atoms/carousel-item/carouselItem";
+import Carousel from "./components/molecules/carousel/carousel";
+import slider1 from "./Imagenes/Slider1.jpg"
+import slider2 from "./Imagenes/Slider2.jpg"
+import slider3 from "./Imagenes/Slider3.jpg"
 
 
 function App() {
   return (
-    <div>Hola</div>
-  );
+    <div>
+      <Carousel>
+        <CarouselItem><img src={slider1} style={{width: "100%"}}/></CarouselItem>
+        <CarouselItem><img src={slider2} style={{width: "100%"} }/></CarouselItem>
+        <CarouselItem><img src={slider3} style={{width: "100%"} }/></CarouselItem>
+      </Carousel>
+    </div>
+  );  
 }
 
 export default App;
