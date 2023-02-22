188f08fcf6c5f097bcbaa661bf2162ac8654335c
SharonInfante <sharonshabel@gmail.com>
2023-02-22 20:27:41 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import ActionCard from "./components/molecules/ActionCard";
 import home from "./assets/img/home..jpg"
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <div>Hola</div>
-      <ActionCard
+    <div className="app">
+      <BrowserRouter></BrowserRouter>      
+    </div>
+  );
+}
+
+export default App;
+
+{/* <ActionCard
         img={home} 
         price="200.000€" 
         direc="Promoción en C. Londres" 
         m="100m" 
         hab="1 dorm." 
         bath="1 baño"
-      />
-    </>
-  );
-}
-
-export default App;
+      /> */}
