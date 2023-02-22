cc4b4ec2552c3b56228ff0f004316358dd0ad4e4
AldaraMG~ <almaga71@gmail.com>
2023-02-22 21:20:16 +0100

    new navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import "bootstrap/dist/css/bootstrap.css";
-import NavbarC from "./components/molecules/NavbarC";
-import Footer from "./components/molecules/Footer";
+
 
 function App() {
- return(
-  <NavbarC/>
- )
+
 }
 
 export default App;
