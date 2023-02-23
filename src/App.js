cb06d26dfb38401f978d0beef4df50955fadecd8
AldaraMG~ <almaga71@gmail.com>
2023-02-23 10:20:37 +0100

    add buttons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import "bootstrap/dist/css/bootstrap.css";
-import Buttons from "./components/atoms/Buttons";
-import Buttons2 from "./components/atoms/Buttons2";
+
 
 
 function App() {
-return(
-  <><Buttons /><Buttons2 /></>
-)
+
 }
 
 export default App;
