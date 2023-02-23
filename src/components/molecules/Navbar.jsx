bc4aa533065794ec36cbc43b207c46a42f59321b
Ashur19 <iglesiasjota1911@gmail.com>
2023-02-23 12:02:30 +0100

    Responsive 1st Commit

--- src/components/molecules/Navbar.jsx
+++ src/components/molecules/Navbar.jsx
@@ -9,23 +9,28 @@ const Navbar = () => {
         <nav className="navContainer" >
             <div className="navbar">
                 <img className="logoEasy" src={Logo} alt="LogoEasy"></img>
-                <h2 className="navbarLog"> Iniciar sesión </h2>
-                <form className="dFlex" role="search">
-                    <input className="form-control me-2" type="search" placeholder="Buscar..." aria-label="Search"></input>
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        class="input-icon"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                    >
-                        <path
-                            fill-rule="evenodd"
-                            d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                            clip-rule="evenodd"
-                        />
-                    </svg>
-
-                </form>
+                <div className='userInterface'>
+                    <a className="navbarLog" href=''> Iniciar sesión </a>
+                    <form className="dFlex" role="search">
+                        <label className='searchContainer'>
+                            <input className="form-control" type="search" placeholder="Buscar..." aria-label="Search"></input>
+                            <span>
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    class="input-icon"
+                                    viewBox="0 0 20 20"
+                                    fill="currentColor"
+                                >
+                                    <path
+                                        fill-rule="evenodd"
+                                        d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                                        clip-rule="evenodd"
+                                    />
+                                </svg>
+                            </span>
+                        </label>
+                    </form>
+                </div>
             </div>
         </nav>
     )
