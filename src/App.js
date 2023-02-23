998f872019e112267e46f807587719eae10ad169
AidaSousa <117081796+AidaSousa@users.noreply.github.com>
2023-02-23 22:17:40 +0100

    integracion

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import ViewDescription4 from "./components/views/viewDescription/ViewDescription
 import Form from "./components/views/form/Form";
 import Contact from "./components/views/Contact";
 import Publish from "./components/views/publish/Publish";
+import Register from "./components/views/register/Register";
 
 function App() {
   return (
@@ -24,6 +25,7 @@ function App() {
           <Route path='/form' element={<Form/>}/>
           <Route path='/contact' element={<Contact/>}/>
           <Route path='/publish' element={<Publish/>}/>
+          <Route path='/register' element={<Register/>}/>
         </Routes>
         <Footer/>
       </BrowserRouter>      
--- src/components/views/form/Form.jsx
+++ src/components/views/form/Form.jsx
@@ -1,18 +1,32 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styles from './form.module.css'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 export default function Form() {
+  const [formData, setFormData] = useState({email:"", password:""})
+  const handleChange = (event)=>{setFormData({...formData, [event.target.name]:event.target.value})}
+  const navigate = useNavigate()
+  const handleSubmit = async(event)=>{event.preventDefault()
+     try{
+    const response = await axios.post("http://localhost:8000/api/login",formData)
+    console.log(response.data)
+    navigate("/")
+      } catch(error){
+        console.log(error.response.data)
+      }
+  }
   return (
     <div className={styles.contactContainer}>
         <h2 className={styles.contactHeading}>Inicia sesión</h2>
-        <form className={styles.contactForm}>
+        <form onSubmit={handleSubmit} className={styles.contactForm}>
             <div className={styles.input}>
                 <label className={styles.formLabel} htmlFor='mailInput'>Email</label>
-                <input className={styles.mailInput} type="email" id='mailInput'/>
+                <input name="email" onChange={handleChange} value={formData.email} className={styles.mailInput} type="email" id='mailInput'/>
             </div>    
             <div className={styles.input}>
                 <label className={styles.formLabel} htmlFor='passwordInput'>Password</label>
-                <input className={styles.password} type="text" id='passwordInput'/>
+                <input name="password" onChange={handleChange} value={formData.password} className={styles.password} type="text" id='passwordInput'/>
             </div>
             <button className={styles.contactSubmit}type='submit'>Continuar</button>
         </form>
