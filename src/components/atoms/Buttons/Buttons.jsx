a1786493fa284789535390df69748077f8338b0c
SharonInfante <sharonshabel@gmail.com>
2023-02-23 16:08:28 +0100

    updated routes in app.js and removed purple links

---
+++ src/components/atoms/Buttons/Buttons.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../Buttons/Buttons.css';
+
+function Buttons() {
+  return (
+    <div className="buttonsCard">
+      <button class="btn btn-outline-success me-2" id="btn-C" >Contactar</button>
+    </div>
+
+  )
+}
+
+export default Buttons
--- src/components/views/home/Home.jsx
+++ src/components/views/home/Home.jsx
@@ -11,6 +11,7 @@ import slider2 from "../../../assets/img/Slider2.jpg"
 import slider3 from "../../../assets/img/Slider3.jpg";
 import "../home/home.css"
 import { Link } from 'react-router-dom';
+import ButtonsAdd from '../../atoms/Buttons/ButtonsAdd';
 
 function Home() {
     
@@ -25,7 +26,7 @@ function Home() {
             </Carousel>
         </div>
         <div className='containerCards'>
-            <Link to="/ViewDescription1">
+            <Link to="/ViewDescription1" className='linkCardHome'>
                 <ActionCard
                     img={Card1} 
                     price="200.000€" 
@@ -35,7 +36,7 @@ function Home() {
                     bath="1 baño"
                 /> 
             </Link>
-            <Link to="/ViewDescription2">
+            <Link to="/ViewDescription2" className='linkCardHome'>
                 <ActionCard
                     img={Card2} 
                     price="200.000€" 
@@ -45,7 +46,7 @@ function Home() {
                     bath="1 baño"
                 />
             </Link>
-            <Link to="/ViewDescription3">
+            <Link to="/ViewDescription3" className='linkCardHome'>
                 <ActionCard
                     img={Card3} 
                     price="200.000€" 
@@ -55,7 +56,7 @@ function Home() {
                     bath="1 baño"
                 /> 
             </Link>
-            <Link to="/ViewDescription4">  
+            <Link to="/ViewDescription4" className='linkCardHome'>  
                 <ActionCard
                     img={Card4} 
                     price="200.000€" 
@@ -65,6 +66,9 @@ function Home() {
                     bath="1 baño"
                 />
             </Link>
+        </div>
+        <div>
+            <Link to="/publish" className='linkCard'><ButtonsAdd/></Link>
         </div>
     </div>
 
