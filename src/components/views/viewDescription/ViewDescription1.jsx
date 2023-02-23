5c480a3362aa8bbbb7d82629e15c2abac4af9efc
Ashur19 <iglesiasjota1911@gmail.com>
2023-02-23 20:28:56 +0100

    Icon Commit

--- src/components/views/viewDescription/ViewDescription1.jsx
+++ src/components/views/viewDescription/ViewDescription1.jsx
@@ -51,7 +51,7 @@ function ViewDescription1() {
                         Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen.
                     </Typography>
                     <br/>
-                    <Link to="/contact" className='linkCard' style={{display: 'flex', justifyContent: 'center'}}><Buttons onClick={()=>mostrarAlerta()} /></Link>
+                    <Link onClick={()=>mostrarAlerta()} to="/contact" className='linkCard' style={{display: 'flex', justifyContent: 'center'}}><Buttons/></Link>
                 </CardContent>
             </Card>
         </div>
