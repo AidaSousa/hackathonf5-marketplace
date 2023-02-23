dbf8960dc787bcab01cd12d02c62c7945c122f82
AldaraMG~ <almaga71@gmail.com>
2023-02-23 10:06:04 +0100

    add buttons

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import "bootstrap/dist/css/bootstrap.css";
+import Buttons from "./components/atoms/Buttons";
+import Buttons2 from "./components/atoms/Buttons2";
 
 
 function App() {
-
+return(
+  <><Buttons /><Buttons2 /></>
+)
 }
 
 export default App;
---
+++ src/components/atoms/Buttons2.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../atoms/Buttons.css'
+
+function Buttons2() {
+  return (
+    
+    <button class="btn btn-outline-success me-2" id="btn-C1" >Continuar</button>
+   
+  )
+}
+
+export default Buttons2
